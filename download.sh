f38a499ea7a209180212c9b000c205b33d23f92d
kennethli319 <liwangyau@gmail.com>
2024-03-21 17:58:18 +0000

    update download , lms

--- download.sh
+++ download.sh
@@ -6,8 +6,9 @@ wget -O "competitionData_readme.txt" https://datadryad.org/stash/downloads/file_
 wget -O "competitionData.tar.gz" https://datadryad.org/stash/downloads/file_stream/2547369
 tar -xzf "competitionData.tar.gz"
 
-# wget -O "languageModel_readme.txt" https://datadryad.org/stash/downloads/file_stream/2547360
-# wget -O "languageModel.tar.gz" https://datadryad.org/stash/downloads/file_stream/2547356
-# tar -xzf "languageModel.tar.gz"
-# wget -O "languageModel_5gram.tar.gz" https://datadryad.org/stash/downloads/file_stream/2547359
-# tar -xzf "languageModel_5gram.tar.gz"
+wget -O "languageModel_readme.txt" https://datadryad.org/stash/downloads/file_stream/2547360
+wget -O "languageModel.tar.gz" https://datadryad.org/stash/downloads/file_stream/2547356
+tar -xzf "languageModel.tar.gz"
+wget -O "languageModel_5gram.tar.gz" https://datadryad.org/stash/downloads/file_stream/2547359
+tar -xzf "languageModel_5gram.tar.gz"
+tar -xzf "languageModel_5gram.tar.gz"
