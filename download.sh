6530592b311c6b2447907f044bcf28d1c8888d6e
kennethli319 <liwangyau@gmail.com>
2024-03-21 16:24:20 +0000

    update download

--- download.sh
+++ download.sh
@@ -1,7 +1,13 @@
 mkdir -p data && cd data
 
-# Download the data (essential)
+# # Download the data (essential)
 wget -O "README.md" https://datadryad.org/stash/downloads/file_stream/2547377
 wget -O "competitionData_readme.txt" https://datadryad.org/stash/downloads/file_stream/2547362
 wget -O "competitionData.tar.gz" https://datadryad.org/stash/downloads/file_stream/2547369
 tar -xzf "competitionData.tar.gz"
+
+# wget -O "languageModel_readme.txt" https://datadryad.org/stash/downloads/file_stream/2547360
+# wget -O "languageModel.tar.gz" https://datadryad.org/stash/downloads/file_stream/2547356
+# tar -xzf "languageModel.tar.gz"
+# wget -O "languageModel_5gram.tar.gz" https://datadryad.org/stash/downloads/file_stream/2547359
+# tar -xzf "languageModel_5gram.tar.gz"
