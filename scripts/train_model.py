7b42008344a2a44f7664e3e7ed1bfed8f8053734
kennethli319 <liwangyau@gmail.com>
2024-03-21 04:50:01 +0000

    update train

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,9 +1,8 @@
-
-modelName = 'speechBaseline4'
+modelName = 'speechBaseline7'
 
 args = {}
-args['outputDir'] = '/oak/stanford/groups/henderj/stfan/logs/speech_logs/' + modelName
-args['datasetPath'] = '/oak/stanford/groups/henderj/fwillett/speech/ptDecoder_ctc'
+args['outputDir'] = './logs/speech_logs/' + modelName
+args['datasetPath'] = './data/ptDecoder_ctc'
 args['seqLen'] = 150
 args['maxTimeSeriesLen'] = 1200
 args['batchSize'] = 64
@@ -12,7 +11,7 @@ args['lrEnd'] = 0.02
 args['nUnits'] = 1024
 args['nBatch'] = 10000 #3000
 args['nLayers'] = 5
-args['seed'] = 0
+args['seed'] = 3
 args['nClasses'] = 40
 args['nInputFeatures'] = 256
 args['dropout'] = 0.4
